d14a6ad31fc63e6e15327a206b29f5b181a38219
Henrique Misael Machado <henrique.nolear@hotmail.com>
2019-05-02 20:09:41 -0300

    Initial configuration: Changed the react app files location for a better design

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './main/webapp/App';
+import * as serviceWorker from './main/webapp/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
